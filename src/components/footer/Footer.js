17f1ca1654c6f062d170118fc0622fb7fc14abdb
Пользователь <jumanazarovdiyorbek270@gmail.com>
2024-02-04 08:56:29 +0500

    imtixon

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,9 @@ import { CiFacebook } from "react-icons/ci";
 import { CiTwitter } from "react-icons/ci";
 import { PiTiktokLogo } from "react-icons/pi";
 import { FiYoutube } from "react-icons/fi";
+import { FaPlus } from "react-icons/fa6";
 
+import r from "../../assets/hyperx.png"
 function Footer() {
   return (
     <div className='footer'>
@@ -31,7 +33,42 @@ function Footer() {
           </div>
          </div>
         </div>
-        
+        <div className="footer__midle">
+<div className="footer__collection">
+  <h2>Support</h2>
+  <h3>  <FaPlus />
+</h3>
+  <p>Product Support</p>
+  <p>Contact Support</p>
+  <p>Shipping Information</p>
+  <p>Refund Policy</p>
+
+</div>
+<div className="footer__collection compony">
+  <h2>Company</h2>
+  <FaPlus />
+  <p>About HyperX</p>
+  <p>Limited Warranty Statement</p>
+  <p>Terms and conditions of Sales <br /> and Service</p>
+  <p>Personal Data Rights Notice</p>
+  <p>Privacy Statement</p>
+  <p>Refund Policy</p>
+  <p>Use of Cookies</p>
+  <p>Accessibility</p>
+  <p>Careers</p>
+
+</div>
+<div className="footer__collection blog">
+  <h2>Other Resources</h2>
+  <FaPlus />
+  <p>NGENUITY Software</p>
+  <p>Blog</p>
+
+</div>
+<div className="footer__midle-img">
+<img src={r} alt="" />
+</div>
+        </div>
     </div>
   )
 }
